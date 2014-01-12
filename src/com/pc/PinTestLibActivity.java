7a91bdf194a8eff756a902f5954305e159077485
Parag Chauhan <parag.chauhan2010@gmail.com>
2014-01-12 14:33:15 +0300

    Test
    Test

---
+++ src/com/pc/PinTestLibActivity.java
@@ -0,0 +1,13 @@
+package com.pc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PinTestLibActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
